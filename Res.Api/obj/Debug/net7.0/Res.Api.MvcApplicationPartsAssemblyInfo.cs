89406604f1f77f13abe50f3b6fbf12bba635826f
unknown <ivangpe00@hotmail.com>
2023-10-08 17:57:17 -0600

    build proyect

--- Res.Api/obj/Debug/net7.0/Res.Api.MvcApplicationPartsAssemblyInfo.cs
+++ Res.Api/obj/Debug/net7.0/Res.Api.MvcApplicationPartsAssemblyInfo.cs
@@ -12,6 +12,7 @@ using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("FluentValidation.AspNetCore")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.OpenApi")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Res.Application")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Res.Common")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Res.Domain")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Res.Infrastructure")]
