802455f107ad85f71a36e3b0388f59bd02c298a0
DarkCrones1 <ivangpe00@hotmail.com.com>
2023-10-19 16:41:40 -0600

    Add StartUp configuration, Program and entities

---
+++ Res.Api/Startup.cs
@@ -0,0 +1,185 @@
+using System.Reflection;
+using System.Text;
+using System.Text.Json.Serialization;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Res.Infrastructure.Filters;
+using Res.Infrastructure.Data;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
+
+namespace Res.Api;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+    private TimeZoneInfo timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time (Mexico)");
+
+    public Startup(IConfiguration configuration)
+    {
+        this._configuration = configuration;
+    }
+
+    public IConfiguration Configuration => _configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers(
+            options =>
+            {
+                options.Filters.Add<GlobalExceptionFilter>();
+            }
+        )
+            .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+            options.JsonSerializerOptions.PropertyNameCaseInsensitive = false;
+            options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            options.JsonSerializerOptions.WriteIndented = true;
+        });
+
+        // Add swagger
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "Res Project API", Version = "v1" });
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlFilePath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            options.IncludeXmlComments(xmlFilePath);
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement{
+        {
+            new OpenApiSecurityScheme{
+                Reference = new OpenApiReference{
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+        });
+
+        services.AddEndpointsApiExplorer();
+
+        // Add DB Connection string
+        services.AddDbContext<ResDBContext>(options =>
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("resDevString") ?? throw new InvalidOperationException("Database conecction string not found...")).UseLazyLoadingProxies();
+        });
+
+        // Add Mappers
+        // services.AddAutoMapper(typeof().Assembly);
+
+        // Configure Cores
+        services.AddCors(options => options.AddPolicy("corsPollicy", builder => {
+            builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+        }));
+        
+        // Add Repositories
+
+        // Add Serivces
+
+        // Add AutoValidator
+
+        // Add Cashing
+        services.AddResponseCaching();
+
+        // Add JWT
+        services.AddAuthentication(opttions => {
+            opttions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            opttions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options => {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = Configuration["Authentication:Issuer"],
+                ValidAudience = Configuration["Authentication:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Authentication:SecretKey"]!))
+            };
+        });
+
+        // Add
+        services.Configure<RequestLocalizationOptions>(options =>
+        {
+            options.DefaultRequestCulture = new RequestCulture("es-MX");
+            options.SupportedCultures = new List<CultureInfo> { new CultureInfo("es-MX") };
+            options.RequestCultureProviders.Clear();
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+    {
+        //app.UseLogResponseHttp();
+
+        app.UseCors("corsPolicy");
+
+        app.UseHttpsRedirection();
+
+        // Configure the HTTP request pipeline.
+        if (environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Gem Project API V1");
+            options.RoutePrefix = string.Empty;
+        });
+
+        app.UseRouting();
+
+        app.UseResponseCaching();
+
+        //app.UseRequestLocalization();
+        app.UseRequestLocalization(new RequestLocalizationOptions
+        {
+            DefaultRequestCulture = new RequestCulture("es-MX"),
+            SupportedCultures = new List<CultureInfo>
+            {
+                new CultureInfo("es-MX"),
+            },
+            SupportedUICultures = new List<CultureInfo>
+            {
+                new CultureInfo("es-MX"),
+            },
+            RequestCultureProviders = new List<IRequestCultureProvider>
+            {
+                new QueryStringRequestCultureProvider(),
+                new CookieRequestCultureProvider(),
+                new AcceptLanguageHeaderRequestCultureProvider(),
+            },
+        });
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
