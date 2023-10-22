94cc324f5e07d5dac0e4a9227f12ac27b3120deb
DarkCrones1 <ivangpe00@hotmail.com.com>
2023-10-22 14:25:37 -0600

    Some changes

--- Res.Domain/Entities/CartDrink.cs
+++ Res.Domain/Entities/CartDrink.cs
@@ -1,14 +1,14 @@
-using Res.Common.Entities;
+// using Res.Common.Entities;
 
-namespace Res.Domain.Entities;
+// namespace Res.Domain.Entities;
 
-public partial class CartDrink : BaseEntityPagination
-{
-    public int CartId { get; set; }
+// public partial class CartDrink : BaseEntityPagination
+// {
+//     public int CartId { get; set; }
 
-    public int DrinkId { get; set; }
+//     public int DrinkId { get; set; }
 
-    public virtual Cart Cart { get; set; } = null!;
+//     public virtual Cart Cart { get; set; } = null!;
 
-    public virtual Drink Drink { get; set; } = null!;
-}
+//     public virtual Drink Drink { get; set; } = null!;
+// }
